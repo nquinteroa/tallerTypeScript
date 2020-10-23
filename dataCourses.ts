d813e2bad919cf8f1735ef0529d555a3f62dd2df
BeastNico <n.quinteroa@uniandes.edu.co>
2020-10-22 22:19:09 -0500

    Initial commit

---
+++ dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("CBU sobre la naturaleza y falsedad humana", "Jesucristo", 4),
+  new Course("Futbol XV", "Jose Mourinho", 2),
+  new Course("Algoritmos para volverse crack", "Mark Zucaritas", 2),
+  new Course("Estructuras de Datos", "CarlosGODLozano", 1),
+  new Course("Como hablar fluido en rueda de prensa", "James Rodriguez", 6)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("CBU sobre la naturaleza y falsedad humana", "Jesucristo", 4),
+    new Course("Futbol XV", "Jose Mourinho", 2),
+    new Course("Algoritmos para volverse crack", "Mark Zucaritas", 2),
+    new Course("Estructuras de Datos", "CarlosGODLozano", 1),
+    new Course("Como hablar fluido en rueda de prensa", "James Rodriguez", 6)
+];
